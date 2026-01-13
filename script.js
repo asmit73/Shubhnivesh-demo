0ec0706201776455acafb1499baa260383a1ac3f
Asmit kumar Dubey <asmitkumar73@gmail.com>
2026-01-13 16:56:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const slides = document.querySelectorAll('.slide');
+/* HAMBURGER MENU */
+function toggleMenu(){
+  const menu = document.getElementById("menu");
+  const burger = document.querySelector(".hamburger");
+
+  if(menu.style.display === "flex"){
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+
+  burger.classList.toggle("active");
+}
+
+/* HERO SLIDER */
+const slides = document.querySelectorAll(".slide");
 let index = 0;
 
 setInterval(() => {
-  slides[index].classList.remove('active');
+  slides[index].classList.remove("active");
   index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
+  slides[index].classList.add("active");
 }, 3000);
