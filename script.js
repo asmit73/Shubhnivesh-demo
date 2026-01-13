fe7dfa603bc8f5392ed4cfb4d2403869d632d139
Asmit kumar Dubey <asmitkumar73@gmail.com>
2026-01-13 16:37:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-/* HERO SLIDER */
-let slides=document.querySelectorAll(".slide");
-let i=0;
-setInterval(()=>{
-  slides.forEach(s=>s.classList.remove("active"));
-  i=(i+1)%slides.length;
-  slides[i].classList.add("active");
-},4000);
-
-/* REVIEWS SLIDER */
-/* ===== VERTICAL CLIENT REVIEWS SLIDER ===== */
-const reviewTrack = document.querySelector(".review-track");
-const reviews = document.querySelectorAll(".review-card");
-let currentReview = 0;
+const slides = document.querySelectorAll('.slide');
+let index = 0;
 
 setInterval(() => {
-  currentReview = (currentReview + 1) % reviews.length;
-  reviewTrack.style.transform =
-    `translateY(-${currentReview * 100}%)`;
-}, 3000); // 3 seconds
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 3000);
